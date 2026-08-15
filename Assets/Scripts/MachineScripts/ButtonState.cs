68b6f37c960b4df8942e47956bfc173f4b8e2d3f
Robby <laura.delfairiere@gmail.com>
2026-08-15 13:04:53 +0200

    cleaning script button

--- Assets/Scripts/MachineScripts/ButtonState.cs
+++ Assets/Scripts/MachineScripts/ButtonState.cs
@@ -3,7 +3,12 @@ using UnityEngine.InputSystem;
 
 public class ButtonState : MonoBehaviour
 {
-    
+    public enum button
+    {
+        Left,
+        Right,
+        Big
+    }
     
     public bool isSurvol = false;
     public bool IsPressed { get; set; } = false;
