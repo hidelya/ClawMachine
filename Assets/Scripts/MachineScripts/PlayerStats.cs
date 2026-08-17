9d1faf20e35815d62a53ce2fa164580d8f1bee62
Robby <laura.delfairiere@gmail.com>
2026-08-17 21:58:10 +0200

    Add synchro text coins

--- Assets/Scripts/MachineScripts/PlayerStats.cs
+++ Assets/Scripts/MachineScripts/PlayerStats.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
@@ -5,10 +6,12 @@ public class PlayerStats : MonoBehaviour
     [SerializeField] private GameObject buttonCoin;
     [SerializeField] private int playerCoins = 100;
 
+    [SerializeField] private TextMeshProUGUI coinText;
    
     public void AddCoins(int amount)
     {
         playerCoins += amount;
+        coinText.text = playerCoins.ToString() + " Coins";
     }
 
     public void RemoveCoins(int amount)
@@ -17,6 +20,8 @@ public class PlayerStats : MonoBehaviour
         { 
             playerCoins -= amount;
             Debug.Log("Removed " + amount + " coins. Remaining coins: " + playerCoins);
+            coinText.text = playerCoins.ToString() + " Coins";
+
         }
         else
         {
